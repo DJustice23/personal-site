b9d0f45adec91fa4bac924547a83ab6a101dfcfd
DJustice23 <djustice@boomtownroi.com>
2022-04-06 09:47:31 -0700

    trying to fix deploy

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
-import Education from '../components/Resume/Education';
-import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
-import References from '../components/Resume/References';
+// import Education from '../components/Resume/Education';
+// import Experience from '../components/Resume/Experience';
+// import Skills from '../components/Resume/Skills';
+// import Courses from '../components/Resume/Courses';
+// import References from '../components/Resume/References';
 
-import courses from '../data/resume/courses';
-import degrees from '../data/resume/degrees';
-import positions from '../data/resume/positions';
-import { skills, categories } from '../data/resume/skills';
+// import courses from '../data/resume/courses';
+// import degrees from '../data/resume/degrees';
+// import positions from '../data/resume/positions';
+// import { skills, categories } from '../data/resume/skills';
 
 const sections = [
   'Education',
@@ -40,11 +40,11 @@ const Resume = () => (
 
         </div>
       </header>
-      <Education data={degrees} />
-      <Experience data={positions} />
-      <Skills skills={skills} categories={categories} />
-      <Courses data={courses} />
-      <References />
+      {/* <Education data='{degrees}' />
+      <Experience data='{positions}' />
+      <Skills skills='{skills}' categories='{categories}' />
+      <Courses data='{courses}' />
+      <References /> */}
 
     </article>
   </Main>
