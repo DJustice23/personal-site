203f4e736ff593ed20191db1d4d51c0c2f3ce340
Justice <asu23justice@gmail.com>
2025-01-16 11:49:54 -0500

    jest config update

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 const config = {
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest', // Use babel-jest to transform JavaScript/TypeScript files
-  },
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'identity-obj-proxy', // Mock CSS modules
     '^.+\\.md$': 'markdown-to-jsx', // Map Markdown imports to markdown-to-jsx
   },
-  testEnvironment: 'jsdom', // Use jsdom for DOM-related tests
 };
 
 module.exports = config;
