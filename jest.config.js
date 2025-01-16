1333452f8f0f7eb430f38d3c3b5f6505a707e9c8
Justice <asu23justice@gmail.com>
2025-01-16 11:52:05 -0500

    jest config update: the sequel

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const config = {
     '^.+\\.(css|less|scss)$': 'identity-obj-proxy', // Mock CSS modules
     '^.+\\.md$': 'markdown-to-jsx', // Map Markdown imports to markdown-to-jsx
   },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
 };
 
 module.exports = config;
