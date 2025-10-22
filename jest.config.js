a79e20aaffa9cae150d4290705790ac6d642c6f3
Justice <asu23justice@gmail.com>
2025-10-21 20:54:48 -0400

    jest updates

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,17 @@ const config = {
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
     '^.+\\.md$': 'markdown-to-jsx',
+    '^unist-util-visit-parents/do-not-use-color$': '<rootDir>/node_modules/unist-util-visit-parents/lib/color.js',
+    '^#minpath$': '<rootDir>/node_modules/vfile/lib/minpath.js',
+    '^#minproc$': '<rootDir>/node_modules/vfile/lib/minproc.js',
+    '^#minurl$': '<rootDir>/node_modules/vfile/lib/minurl.js',
   },
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
+  testEnvironment: 'jsdom',
   transformIgnorePatterns: [
-    '/node_modules/(?!(react-markdown|comma-separated-tokens|hast-util-to-jsx-runtime|vfile|unist-util-.+|micromark.+|devlop)/)',
+    '/node_modules/(?!(react-markdown|remark-.*|rehype-.*|mdast-.*|hast-.*|unist-.*|unified|estree-.*|micromark.*|vfile.*|property-information|space-separated-tokens|comma-separated-tokens|html-url-attributes|web-namespaces|trim-lines|mdurl|markdown-table|character-entities|decode-named-character-reference|zwitch|bail|is-plain-obj|trough|devlop)/)',
   ],
 };
 
